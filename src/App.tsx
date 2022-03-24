3420cdede02ba194aaef331c027fd224e5d76878
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-24 23:07:55 +0800

    add code state

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,18 @@ import QRCode from "react-qr-code";
 function App() {
   const [code, setCode] = useState("");
 
+  const handleChange = (e: any) => {
+    setCode(e.target.value);
+  };
+
   return (
     <div className="App">
       <QRCode value={code} />
-      <input type="text" placeholder="Enter your qrcode value" />
+      <input
+        type="text"
+        placeholder="Enter your qrcode value"
+        onChange={handleChange}
+      />
     </div>
   );
 }
