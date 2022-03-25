922c53ddd8e49bb1530ce8d10388ac7c2b31636b
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-25 10:10:47 +0800

    add col

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container center">
+      <div className="container center col">
         <QRCode value={code} className="QR-CODE" />
         <input
           type="text"
