66ac9de66686abdb815e670c562a66b8ef6d5edb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-25 10:45:16 +0800

    add setImageToSVG

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import QRCode from "react-qr-code";
 
 function App() {
   const [code, setCode] = useState("");
+  const [imgSrc, setSrc] = useState("");
 
   const handleChange = (e: any) => {
     setCode(e.target.value);
@@ -11,6 +12,15 @@ function App() {
 
   const handleDownload = () => {};
 
+  const setImageToSVG = () => {
+    const svg = document.querySelector(".QR-CODE");
+
+    var xml = new XMLSerializer().serializeToString(svg as Node);
+    setSrc("data:image/svg+xml;charset=utf-8," + xml);
+  };
+  useEffect(() => {
+    setImageToSVG();
+  }, []);
   return (
     <div className="App">
       <div className="header">
@@ -18,6 +28,7 @@ function App() {
       </div>
       <div className="container center col">
         <QRCode value={code} className="QR-CODE" />
+        <img src={imgSrc} alt="" />
         <input
           type="text"
           placeholder="Enter your QR code value"
