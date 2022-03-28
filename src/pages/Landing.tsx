edd97e33d2088a24b39adba23f2c39d791f2973a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 13:43:36 +0800

    move navbar

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -32,9 +32,6 @@ export default () => {
 
   return (
     <div className="landing-page">
-      <div className="header">
-        <h1>QR Code Generator</h1>
-      </div>
       <div className="container center col">
         <QRCode
           value={code}
