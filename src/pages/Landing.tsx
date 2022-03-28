a489348bbb43c0ec57d92ff07efc97be3eb50689
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 13:37:58 +0800

    add download function

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,6 +4,7 @@ import "../styles/landing.css";
 
 export default () => {
   const [code, setCode] = useState("");
+  const [title, setTitle] = useState("");
   const [imgSrc, setSrc] = useState("");
 
   const handleChange = (e: any) => {
@@ -11,8 +12,22 @@ export default () => {
   };
 
   const handleDownload = () => {
-    const svg = document.querySelector(".QR-CODE");
-    console.log(svg);
+    const svg = document.querySelector(".QR-CODE") as Node;
+    const svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx?.drawImage(img, 0, 0);
+      const pngFile = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.download = title || "QR Code";
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
   };
 
   return (
@@ -21,7 +36,13 @@ export default () => {
         <h1>QR Code Generator</h1>
       </div>
       <div className="container center col">
-        <QRCode value={code} className="QR-CODE" />
+        <QRCode
+          value={code}
+          className="QR-CODE"
+          title={title}
+          download={<button>Download</button>}
+        />
+
         <input
           type="text"
           placeholder="Enter your QR code value"
