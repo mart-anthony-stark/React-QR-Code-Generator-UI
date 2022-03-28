baf5328bf1e25c2de2d5576263ba4dc43dc97cdb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 21:12:15 +0800

    add accept cookies

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Navbar from "./components/Navbar";
+import AcceptCookies from "./components/AcceptCookies";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
         </Routes>
       </Router>
+      <AcceptCookies />
     </div>
   );
 }
