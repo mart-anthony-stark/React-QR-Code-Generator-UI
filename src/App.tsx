5aadb8034b0aa98f595a314019932ed6beaecc09
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 13:43:27 +0800

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
