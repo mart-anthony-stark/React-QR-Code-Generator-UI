fb387c86ad58f18c470108e6055b1ff262797181
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 21:12:07 +0800

    renamed

---
+++ src/components/AcceptCookies.tsx
@@ -0,0 +1,14 @@
+import "../styles/allow-cookies.css";
+
+export default () => {
+  return (
+    <div className="accept-cookies">
+      <h2>This website uses cookies</h2>
+      <p>
+        We rely on cookies to remember your preferences and to analyze our
+        website traffic. By clicking, you agree to our Privacy Policy
+      </p>
+      <button className="cta">Allow Cookies</button>
+    </div>
+  );
+};
