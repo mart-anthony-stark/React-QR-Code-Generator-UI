328ea5cc7379cdc5ebea8c6760a625dda2040f4f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-28 20:51:59 +0800

    add links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,11 @@ export default () => {
   return (
     <div className="header">
       <h1>QR Code Generator</h1>
+
+      <div className="links">
+        <button className="rounded">Login</button>
+        <button className="rounded cta">Signup</button>
+      </div>
     </div>
   );
 };
