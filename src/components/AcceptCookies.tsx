06701611603111667a84e620f89b6aadaa56f341
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 15:28:27 +0800

    add props

--- src/components/AcceptCookies.tsx
+++ src/components/AcceptCookies.tsx
@@ -1,6 +1,14 @@
 import "../styles/allow-cookies.css";
+import { FC } from "react";
 
-export default () => {
+type Props = {
+  setVisible: (isVisible: boolean) => void;
+};
+const AcceptCookies: FC<Props> = ({ setVisible }) => {
+  const cancelCookie = () => {
+    setVisible(false);
+  };
+
   return (
     <div className="accept-cookies">
       <h2>This website uses cookies</h2>
@@ -10,8 +18,10 @@ export default () => {
       </p>
       <div className="flex">
         <button className="cta">Allow Cookies</button>
-        <button>Cancel</button>
+        <button onClick={cancelCookie}>Cancel</button>
       </div>
     </div>
   );
 };
+
+export default AcceptCookies;
