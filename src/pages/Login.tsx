6f301c299dbdf56783518427ea6874de2c5309e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 19:35:15 +0800

    add form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,16 @@
 const Login = () => {
-  return <div className="login"></div>;
+  return (
+    <div className="login">
+      <form>
+        <div className="field">
+          <input type="text" placeholder="johndoe@gmail.com" />
+        </div>
+        <div className="field">
+          <input type="text" placeholder="your password" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
