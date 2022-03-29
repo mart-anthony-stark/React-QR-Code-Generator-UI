5cb170c3890658d61325bd914dd72d32b477a63f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 20:48:33 +0800

    add storee

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </Provider>,
+  document.getElementById("root")
+);
