36ef113c6abe02e77f18650349e52549d1d5dbfd
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-29 19:48:58 +0800

    add handle submit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "../styles/login.css";
 
 const Login = () => {
   const [btnDisabled, setbtnDisabled] = useState(false);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field">
           <input type="text" placeholder="johndoe@gmail.com" />
         </div>
         <div className="field">
           <input type="text" placeholder="your password" />
         </div>
+        <button disabled={btnDisabled} className="cta">
+          LOGIN
+        </button>
       </form>
-      <button disabled={btnDisabled}></button>
     </div>
   );
 };
