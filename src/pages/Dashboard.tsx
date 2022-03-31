e22300be3e6e473bd491780084f9cdabae198bbb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 19:18:06 +0800

    add add icon

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
 import "../styles/dashboard.css";
 
 const Dashboard = () => {
-  return <div className="dashboard"></div>;
+  return (
+    <div className="dashboard">
+      <div className="add-icon">
+        <div className="plus">+</div>
+        <h4>Add New QR Code</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
