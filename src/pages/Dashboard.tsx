216a8adeeade22def0db601435e3ba1c105c69ca
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 20:28:46 +0800

    add getItems method

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
+import { useSelector } from "react-redux";
 import QRItem from "../components/QRItem";
 import "../styles/dashboard.css";
 
 const Dashboard = () => {
+  const [qrCodes, setQrCodes] = useState();
+  const user = useSelector((state: any) => state.user);
+
+  const getItems = async () => {
+    const token = localStorage.getItem("token");
+    const endpoint = `${import.meta.env.VITE_API_BASE_URL}/qr/all/${user._id}`;
+    console.log(endpoint);
+    const res = await fetch(endpoint, {
+      headers: { token: `bearer ${token}` },
+    });
+
+    const data = await res.json();
+    setQrCodes(data);
+  };
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return (
     <div className="dashboard">
       <div className="add-icon">
