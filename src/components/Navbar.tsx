045fb6596472d05716481b9089610e229f0f10ea
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 10:02:23 +0800

    hide register links when logged

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,25 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default () => {
+  const user = useSelector((state: any) => state.user);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>QR Code Generator</h1>
       </Link>
 
-      <div className="links">
-        <Link to="/login">
-          <button className="rounded">Login</button>
-        </Link>
-        <Link to="/register">
-          <button className="rounded cta">Signup</button>
-        </Link>
-      </div>
+      {!user && (
+        <div className="links">
+          <Link to="/login">
+            <button className="rounded">Login</button>
+          </Link>
+          <Link to="/register">
+            <button className="rounded cta">Signup</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
