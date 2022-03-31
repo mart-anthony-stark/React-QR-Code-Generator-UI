c7fbd633dc6209e7cda95003536ad13e92f3559a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-31 20:40:58 +0800

    add AddIcon component

---
+++ src/components/AddIcon.tsx
@@ -0,0 +1,12 @@
+import '../styles/add-icon.css'
+
+const AddIcon = () => {
+  return (
+    <div className="add-icon">
+      <div className="plus">+</div>
+      <h4>Add New QR Code</h4>
+    </div>
+  );
+};
+
+export default AddIcon
