4a8e127ea4532a8d7dc7c83af1d44918953deb50
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 20:43:06 +0800

    change tag

--- src/components/QRItem.tsx
+++ src/components/QRItem.tsx
@@ -12,7 +12,7 @@ const QRItem: FC<Props> = ({ title, value }) => {
   return (
     <div className="item">
       <QRCode className="qr" size={150} value={value} />
-      <h3 className="title">{title}</h3>
+      <p className="title">{title}</p>
     </div>
   );
 };
