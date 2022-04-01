da70b6b326d98c8c2644d05f5cb842f7f683633a
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 20:55:52 +0800

    codesplit handledownload method

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,33 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import QRCode from "react-qr-code";
 import "../styles/landing.css";
+import { handleDownload } from "../utils/download";
 
 export default () => {
   const [code, setCode] = useState("");
   const [title, setTitle] = useState("");
-  const [imgSrc, setSrc] = useState("");
 
   const handleChange = (e: any) => {
     setCode(e.target.value);
-  };
-
-  const handleDownload = () => {
-    const svg = document.querySelector(".QR-CODE") as Node;
-    const svgData = new XMLSerializer().serializeToString(svg);
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    const img = new Image();
-    img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx?.drawImage(img, 0, 0);
-      const pngFile = canvas.toDataURL("image/png");
-      const downloadLink = document.createElement("a");
-      downloadLink.download = title || "QR Code";
-      downloadLink.href = `${pngFile}`;
-      downloadLink.click();
-    };
-    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
   };
 
   return (
@@ -45,7 +26,9 @@ export default () => {
           placeholder="Enter your QR code value"
           onChange={handleChange}
         />
-        <button onClick={handleDownload}>Download QR Code</button>
+        <button onClick={() => handleDownload({ selector: ".QR-CODE" })}>
+          Download QR Code
+        </button>
       </div>
     </div>
   );
