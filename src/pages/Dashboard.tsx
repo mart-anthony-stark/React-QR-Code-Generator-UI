58cd9e996a3a942a223738a2a6cdbfe666805e61
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 19:57:58 +0800

    code splitting

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import QRCode from "react-qr-code";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import AddIcon from "../components/AddIcon";
+import AddItem from "../components/AddItem";
 import Modal from "../components/Modal";
 import QRItem from "../components/QRItem";
 import "../styles/dashboard.css";
@@ -12,11 +13,6 @@ const Dashboard = () => {
   const user = useSelector((state: any) => state.user);
   const [qrCodes, setQrCodes] = useState([]);
   const [showModal, toggleModal] = useState(false);
-  const [addQr, setAddQr] = useState<TQRCode>({
-    title: "",
-    user: user._id,
-    value: "",
-  });
 
   const getItems = async () => {
     const token = localStorage.getItem("token");
@@ -33,55 +29,12 @@ const Dashboard = () => {
     getItems();
   }, []);
 
-  const handleAddItem = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (addQr.title == "" || addQr.user == "" || addQr.value == "") {
-      toast.error("Title and value is required for QR Code");
-    } else {
-      const res = await fetch(import.meta.env.VITE_API_BASE_URL + "/qr", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          token: `bearer ${localStorage.getItem("token")}`,
-        },
-        body: JSON.stringify(addQr),
-      });
-      const data = await res.json();
-      console.log(data);
-      toggleModal(false);
-      setAddQr({ title: "", user: user._id, value: "" });
-      toast.success("QR Code added successfully!");
-      getItems();
-    }
-  };
-
   return (
     <div className="dashboard">
       <AddIcon onClick={() => toggleModal(true)} />
       {showModal && (
         <Modal>
-          <div className="addItem">
-            <h2 className="pri-light">Create New QR Code</h2>
-            <QRCode value={addQr?.value} size={200} />
-            <input
-              type="text"
-              placeholder="Title"
-              onChange={(e) => setAddQr({ ...addQr, title: e.target.value })}
-            />
-            <input
-              type="text"
-              placeholder="Value"
-              onChange={(e) => setAddQr({ ...addQr, value: e.target.value })}
-            />
-
-            <div className="buttons">
-              <button className="cta" onClick={handleAddItem}>
-                ADD
-              </button>
-              <button className="cancel" onClick={() => toggleModal(false)}>
-                Cancel
-              </button>
-            </div>
-          </div>
+          <AddItem toggleModal={toggleModal} getItems={getItems} user={user} />
         </Modal>
       )}
 
