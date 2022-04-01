8104235985e166db83385587fe2fcad9e6a144a5
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 20:50:03 +0800

    add download method

---
+++ src/utils/download.ts
@@ -0,0 +1,23 @@
+type Params = {
+  title?: string;
+  selector: string;
+};
+
+export const handleDownload = ({ title, selector }: Params) => {
+  const svg = document.querySelector(selector) as Node;
+  const svgData = new XMLSerializer().serializeToString(svg);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const img = new Image();
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx?.drawImage(img, 0, 0);
+    const pngFile = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.download = title || "QR Code";
+    downloadLink.href = `${pngFile}`;
+    downloadLink.click();
+  };
+  img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+};
