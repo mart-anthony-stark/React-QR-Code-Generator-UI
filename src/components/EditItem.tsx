8aaf3f3e099d7ef792ba38bf80e4c61b7b5a0dd5
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-01 20:10:52 +0800

    add edit item component

---
+++ src/components/EditItem.tsx
@@ -0,0 +1,64 @@
+import { FC, useState } from "react";
+import QRCode from "react-qr-code";
+import { toast } from "react-toastify";
+import { TQRCode } from "../types/QR";
+
+type Props = {
+  user: any;
+  toggleModal: (state: boolean) => void;
+  getItems: () => void;
+  QR: TQRCode;
+};
+
+const AddItem: FC<Props> = ({ user, toggleModal, getItems, QR }) => {
+  const [editQr, setQR] = useState<TQRCode>(QR);
+
+  const handleAddItem = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(editQr);
+    if (editQr.title == "" || editQr.user == "" || editQr.value == "") {
+      toast.error("Title and value is required for QR Code");
+    } else {
+      const res = await fetch(import.meta.env.VITE_API_BASE_URL + "/qr", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          token: `bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify(editQr),
+      });
+      const data = await res.json();
+      console.log(data);
+      toggleModal(false);
+      setQR({ title: "", user: user._id, value: "" });
+      toast.success("QR Code saved successfully!");
+      getItems();
+    }
+  };
+  return (
+    <div className="addItem">
+      <h2 className="pri-light">Create New QR Code</h2>
+      <QRCode value={editQr?.value} size={200} />
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={(e) => setQR({ ...editQr, title: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Value"
+        onChange={(e) => setQR({ ...editQr, value: e.target.value })}
+      />
+
+      <div className="buttons">
+        <button className="cta" onClick={handleAddItem}>
+          ADD
+        </button>
+        <button className="cancel" onClick={() => toggleModal(false)}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddItem;
