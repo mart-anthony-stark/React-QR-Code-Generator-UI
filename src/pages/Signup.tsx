bc6960d8a76a7a5f7fa86d621c0778df776e54c3
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 19:15:34 +0800

    remove unused hooks

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef, useState } from "react";
+import React, { FC, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/auth.css";
 import "react-toastify/dist/ReactToastify.css";
