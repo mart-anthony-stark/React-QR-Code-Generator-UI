df26a332775044ca141af1c048b69dc99c76e81d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 11:02:08 +0800

    add Subscription component

---
+++ src/pages/Subscription.tsx
@@ -0,0 +1,11 @@
+import "../styles/subscription.css";
+
+const Subscription = () => {
+  return (
+    <div className="subscription">
+      <h1>Choose plan</h1>
+    </div>
+  );
+};
+
+export default Subscription;
