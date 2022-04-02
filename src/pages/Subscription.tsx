9bbcca5055ffdc992b5b9e975a8d2673905dcfd0
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-02 19:44:46 +0800

    add premium pricing card

--- src/pages/Subscription.tsx
+++ src/pages/Subscription.tsx
@@ -7,7 +7,11 @@ const Subscription = () => {
       <div className="top-banner">
         <div className="desc">Membership Pricing</div>
         <h1 className="heading">Choose a plan that's right for you</h1>
-        <PricingCard title="FREE"></PricingCard>
+
+        <div className="pricing-table">
+          <PricingCard title="FREE"></PricingCard>
+          <PricingCard title="PREMIUM"></PricingCard>
+        </div>
       </div>
     </div>
   );
