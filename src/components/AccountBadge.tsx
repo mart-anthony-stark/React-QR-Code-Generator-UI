5622a20ae4ac89b81ebc943ebf12e93de0fd4f0e
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-03 19:08:24 +0800

    add Props type

--- src/components/AccountBadge.tsx
+++ src/components/AccountBadge.tsx
@@ -1,7 +1,12 @@
+import { FC } from "react";
 import "../styles/account-badge.css";
 
-const AccountBadge = () => {
-  return <div className="account-badge"></div>;
+type Props = {
+  type: string;
+};
+
+const AccountBadge: FC<Props> = ({ type }) => {
+  return <div className={`account-badge ${type}`}>{type.toUpperCase()}</div>;
 };
 
 export default AccountBadge;
